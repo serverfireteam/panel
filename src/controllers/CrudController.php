56fb340c61067f3819a8ebbe69b4735e39d7a874
siavash13 <siavashizadxasty@gmail.com>
2014-11-15 18:18:10 +0330

    removing errors PSR2

--- src/controllers/CrudController.php
+++ src/controllers/CrudController.php
@@ -1,15 +1,16 @@
 <?php
 namespace Sadra\Pack1;
 
-use \Illuminate\Routing\Controllers;
-/* 
+use \Illuminate\Routing\Controllers; 
+/*  
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 
-class CrudController extends \Controller {
+class CrudController extends \Controller
+{
 
     
     public $grid;
@@ -18,42 +19,44 @@ class CrudController extends \Controller {
     public $edit;
     public $filter;
     
-    public function __construct() {
+    public function __construct()
+    {
          
-        $params =  \Route::current()->parameters();
         $this->entity = 'users';
        // $this->entity = $params['entity'];
                       
         $base_path = base_path();
         $base_path .= "\\vendor\\serverfireteam\\panel\\src\\views";
-     
         
-        \View::addLocation($base_path);        
-        \View::addNamespace('panelViews', $base_path); 
+        \View::addLocation($base_path);
+        \View::addNamespace('panelViews', $base_path);
     }    
 
     
-    public function all($entity){
+    public function all($entity)
+    {
                                 
                           
         //$this->addStylesToGrid();                     
                    
     }
     
-    public function edit($entity){
+    public function edit($entity)
+    {
         
     }
 
     
 
-    public function addStylesToGrid(){
+    public function addStylesToGrid()
+    {
         
-        $this->grid->edit('edit', 'Edit','show|modify');
-        $this->grid->link('edit',"New Article", "TR");
-        $this->grid->orderBy('id','desc');
+        $this->grid->edit('edit', 'Edit', 'show|modify');
+        $this->grid->link('edit', "New Article", "TR");
+        $this->grid->orderBy('id', 'desc');
         $this->grid->paginate(10);
 
-        $this->grid->row(function ($row) {
+        $this->grid->row(function ($row){
            if ($row->cell('id')->value == 20) {
                $row->style("background-color:#CCFF66");
            } elseif ($row->cell('id')->value > 15) {
@@ -63,18 +66,17 @@ class CrudController extends \Controller {
         });        
     }
 
-    public function returnView(){
-         return \View::make('panelViews::all', array(             
+    public function returnView()
+    {
+         return \View::make('panelViews::all', array(
              'grid' => $this->grid,
-             'filter' => $this->filter 
-        ));  
+             'filter' => $this->filter
+        ));
     }
     
     public function returnEditView(){
          return \View::make('panelViews::edit' , array(
              'edit' => $this->edit            
         )); 
-    }
-    
-    
+    }        
 }
