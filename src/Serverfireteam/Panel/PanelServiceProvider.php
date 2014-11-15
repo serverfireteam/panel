75dc3965192a9417f79fb2944e283d47517768e2
siavash13 <siavashizadxasty@gmail.com>
2014-11-15 19:06:51 +0330

    removing errors PSR2 ServiceProvider

--- src/Serverfireteam/Panel/PanelServiceProvider.php
+++ src/Serverfireteam/Panel/PanelServiceProvider.php
@@ -4,62 +4,44 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\Facades\Route;
 
-class PanelServiceProvider extends ServiceProvider {
-
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-            $this->app->register('Zofe\Rapyd\RapydServiceProvider');
-            
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-           
-
-	}
+class PanelServiceProvider extends ServiceProvider
+{
+    protected $defer = false;
         
-        public function boot(){
-                     
+    public function register()
+    {
+        $this->app->register('Zofe\Rapyd\RapydServiceProvider');
             
-            $this->package('serverfireteam/panel');                                  
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+    }
+        
+    public function boot()
+    {
+        $this->package('serverfireteam/panel');
             
-            Route::get('/panel2/{entity}/all', function($entity){
-                $controller = \App::make('Sadra\\Pack1\\'.$entity.'Controller');
-                return $controller->callAction('all', array('entity' => $entity));
-            });
+        Route::get('/panel2/{entity}/all', function ($entity) {
+            $controller = \App::make('Sadra\\Pack1\\'.$entity.'Controller');
+            return $controller->callAction('all', array('entity' => $entity));
+        });
     
-             Route::any('/panel2/{entity}/edit', function($entity){
-                $controller = \App::make('Sadra\\Pack1\\'.$entity.'Controller');
-                return $controller->callAction('edit', array('entity' => $entity));
-            });
+        Route::any('/panel2/{entity}/edit', function ($entity) {
+            $controller = \App::make('Sadra\\Pack1\\'.$entity.'Controller');
+            return $controller->callAction('edit', array('entity' => $entity));
+        });
            
-            
-            
-           
-              
-            include __DIR__."/../../routes.php";
+                                                 
+        include __DIR__."/../../routes.php";
 
-            AliasLoader::getInstance()->alias('Serverfireteam', 'Serverfireteam\Panel\Serverfireteam');
-        }
+        AliasLoader::getInstance()->alias('Serverfireteam', 'Serverfireteam\Panel\Serverfireteam');
+    }
 
-        /**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
