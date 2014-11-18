3511b11740ba748a32f36a1f4245e25f1b607825
siavash13 <siavashizadxasty@gmail.com>
2014-11-18 19:12:19 +0330

    add admin

---
+++ src/models/Admin.php
@@ -0,0 +1,27 @@
+<?php
+namespace Serverfireteam\Panel;
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Admin extends Eloquent implements UserInterface, RemindableInterface {
+
+	use \UserTrait, \RemindableTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'admins';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password', 'remember_token');
+
+}
