a449d384791c256ed0974944017db2ffd8715ec7
Alireza Aboutalebi <abotalby@gmail.com>
2014-12-15 13:24:53 +0330

    remove extre h1 in edit

--- src/views/edit.blade.php
+++ src/views/edit.blade.php
@@ -1,13 +1,6 @@
 @extends('panelViews::mainTemplate')
 @section('page-wrapper')
 
-
-    <h1>DataEdit</h1>
-    <p>
-
-        {{ $edit }}
-        
-        
-    </p>
+    <p>{{ $edit }}</p>
     
 @stop    
