48bf889797b693916a089f4472741c3d07f22e35
Alireza Aboutalebi <abotalby@gmail.com>
2014-12-31 16:40:48 +0330

    clean the code

--- src/Serverfireteam/Panel/libs/dashboard.php
+++ src/Serverfireteam/Panel/libs/dashboard.php
@@ -7,18 +7,19 @@ class dashboard
     public static function create()
     {
         $config = \Config::get('config.crudItems');
+        $dashboard = array();
         //Make Dashboard Items
-         foreach ( $config as $key => $value ){
-             if(class_exists($value))
+        foreach ( $config as $key => $value ){
+            if(class_exists($value))
                 $dashboard[] = array(
                     'title'=>$key,
                     'count'=> $value::all()->count(),
                     'showListUrl'=>'panel/'.$value.'/all',
                     'addUrl'=>'panel/'.$value.'/edit',
                      );
-             else
-                 throw new \Exception('Model name doesnt match config.crudItems in '.$value);
-         }
+            else
+                throw new \Exception('Model name doesnt match config.crudItems in '.$value);
+        }
          
          return $dashboard;
     }
