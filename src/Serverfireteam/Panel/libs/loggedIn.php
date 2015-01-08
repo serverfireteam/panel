3a98fde0270d2157ae5878e3b5b9c51dd0f01924
unknown <siavashizadxasty@gmail.com>
2015-01-08 17:19:44 +0330

    add loggedin

---
+++ src/Serverfireteam/Panel/libs/loggedIn.php
@@ -0,0 +1,16 @@
+<?php namespace Serverfireteam\Panel\libs;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return [
+    'can_edit' => function() {
+        $temp = \Config::get('auth.model');
+        \Config::set('auth.model', 'Serverfireteam\Panel\Admin');
+        $access = !\Auth::guest();
+        \Config::set('auth.model', $temp);
+        return $access;
+    },
+];
