f6f9bc413814ca1b282398f1b444d16e5b78765c
unknown <siavashizadxasty@gmail.com>
2015-01-10 18:15:23 +0330

    change loggedIn to class

--- src/Serverfireteam/Panel/loggedIn.php
+++ src/Serverfireteam/Panel/loggedIn.php
@@ -1,16 +1,19 @@
 <?php namespace Serverfireteam\Panel;
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-return [
-    'can_edit' => function() {
-        $temp = \Config::get('auth.model');
-        \Config::set('auth.model', 'Serverfireteam\Panel\Admin');
-        $access = !\Auth::guest();
-        \Config::set('auth.model', $temp);
-        return $access;
-    },
-];
+
+class loggedIn{
+
+
+	public function checkLoggedIn(){
+		return [
+		    'can_edit' => function() {
+		        $temp = \Config::get('auth.model');
+		        \Config::set('auth.model', 'Serverfireteam\Panel\Admin');
+		        $access = !\Auth::guest();
+		        \Config::set('auth.model', $temp);
+		        return $access;
+		    },
+		];
+	}
+}
+
