581dd49c9ee57bb92d57992b37d2b2d86dd2899d
unknown <siavashizadxasty@gmail.com>
2015-01-10 19:25:30 +0330

    logged in modified

--- src/Serverfireteam/Panel/libs/AuthAdmin.php
+++ src/Serverfireteam/Panel/libs/AuthAdmin.php
@@ -5,15 +5,13 @@ class AuthAdmin{
 
 
 	public function checkLoggedIn(){
-		return [
-		    'can_edit' => function() {
-		        $temp = \Config::get('auth.model');
-		        \Config::set('auth.model', 'Serverfireteam\Panel\Admin');
-		        $access = !\Auth::guest();
-		        \Config::set('auth.model', $temp);
-		        return $access;
-		    },
-		];
+
+        $temp = \Config::get('auth.model');
+        \Config::set('auth.model', 'Serverfireteam\Panel\Admin');
+        $access = !\Auth::guest();
+        \Config::set('auth.model', $temp);
+        return $access;
+		  
 	}
 }
 
