c71f77ce915d4ff3082cdcfd1a699ffc7ccf7e2a
hamid <xsx.hamid@gmail.com>
2015-01-21 12:56:26 +0330

    User info + visit site link
    User info + visit site link

--- src/routes.php
+++ src/routes.php
@@ -32,7 +32,8 @@ Route::group(array('prefix' => 'panel' ,'before' => 'auth'), function()
         // main page for the admin section (app/views/admin/dashboard.blade.php)
         Route::get('/', function()
         {
-            return View::make('panelViews::dashboard');
+            $grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( Auth::user()->email ) ) ) . "?d=mm&s=128";
+            return View::make('panelViews::dashboard')->with('grav_url',$grav_url);
         });
         
         Route::any('/{entity}/{methods}', function ($entity,$methods) {
@@ -110,7 +111,6 @@ Route::get('/panel/login', function () {
     $message = (Session::has('message') ? Session::get('message') : 'Please Sign In');
     $mesType = (Session::has('mesType') ? Session::get('mesType') : 'message');
     return View::make('panelViews::login')->with('message', $message)->with('mesType', $mesType);
-
 });
 
 
