b932df3709898be9fc558089ed8fea44413d13b2
Serverfire <siavashizadxasty@gmail.com>
2015-01-26 17:52:34 +0330

    finalize view

--- src/lang/de/fields.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'settings' => 'Setting de',
-    'logout' => 'Log out de',
-    'ProfileEdit' => 'Profile Edit de',
-    'ResetPassword' => 'Reset Password de',
-    'Add' => 'ADD de',
-);
