7065a81fa7271ad304f780fb802b3a28a1fe81ea
alenoosh <hope2end@gmail.com>
2015-01-27 13:02:40 +0330

    Google Ads are removed from 404 view.

--- src/views/404.blade.php
+++ src/views/404.blade.php
@@ -94,23 +94,7 @@ animation: AnimationName 10s ease infinite;
 
 
 <body>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-30027142-1', 'w3layouts.com');
-  ga('send', 'pageview');
-</script>
-<script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
-<!-- w3layouts_970x90 -->
-<ins class='adsbygoogle'
-     style='display:block;width:970px;height:90px;margin:0 auto;'
-     data-ad-client='ca-pub-9153409599391170'
-     data-ad-slot='2384823885'></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+
 
 	<div class="wrap">
 	   <div class="logo">
