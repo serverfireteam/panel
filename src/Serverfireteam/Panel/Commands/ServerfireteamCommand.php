cad4ddd444e1fd81bbe2b7c7cabe83b4237e3b27
Alireza Aboutalebi <abotalby@gmail.com>
2015-02-14 11:08:16 +0330

    fix installations

--- src/Serverfireteam/Panel/Commands/ServerfireteamCommand.php
+++ src/Serverfireteam/Panel/Commands/ServerfireteamCommand.php
@@ -38,7 +38,7 @@ class panelCommand extends Command {
        
         $this->info('            [ Wellcome to ServerFireTeam Panel Installations ]       ');
         
-        //$this->call('asset:publish', array('package' => 'serverfireteam/rapyd-laravel'));
+        $this->call('vendor:publish');
         
         $this->call('migrate', array('--path' => 'vendor/serverfireteam/panel/src/database/migrations'));
         
