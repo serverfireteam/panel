b0fd39ba17c45adf14b4502d782a75ecb2ee1ed6
alenoosh <hope2end@gmail.com>
2015-02-15 12:43:04 +0330

    A line related to previous commits was removed by mistake. (Path of Rapyd Translations)

--- src/Serverfireteam/Panel/PanelServiceProvider.php
+++ src/Serverfireteam/Panel/PanelServiceProvider.php
@@ -55,6 +55,7 @@ class PanelServiceProvider extends ServiceProvider
         include __DIR__."/../../routes.php";
 
 	$this->loadTranslationsFrom(base_path() . '/vendor/serverfireteam/panel/src/lang', 'panel');
+        $this->loadTranslationsFrom(base_path() . '/vendor/serverfireteam/rapyd-laravel/src/lang', 'rapyd');
 
         AliasLoader::getInstance()->alias('Serverfireteam', 'Serverfireteam\Panel\Serverfireteam');
     }
