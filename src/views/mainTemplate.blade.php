fd8e5d105f9dd32d3f2666f5ad6a6d805676fe09
hamid <xsx.hamid@gmail.com>
2015-04-05 15:19:14 +0430

    loading ui change
    loading ui change

--- src/views/mainTemplate.blade.php
+++ src/views/mainTemplate.blade.php
@@ -10,8 +10,14 @@ dashboard
  /*--}}         
        
     <div class="loading">
-        <div class="rnd-box"><div class="inner-box"></div></div>
         <h1> LOADING </h1>
+        <div class="spinner">
+          <div class="rect1"></div>
+          <div class="rect2"></div>
+          <div class="rect3"></div>
+          <div class="rect4"></div>
+          <div class="rect5"></div>
+        </div>
     </div>
 
     <div id="wrapper">
