6f673324305893d67c3bcf3d95bede2e20188fad
alenoosh <hope2end@gmail.com>
2015-05-25 13:28:04 +0430

    Links to non-existing files (related to redactor) are removed.

--- src/views/master.blade.php
+++ src/views/master.blade.php
@@ -10,7 +10,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link media="all" type="text/css" rel="stylesheet" href="{{asset("packages/zofe/rapyd/assets/demo/style.css")}}">
-    <link media="all" type="text/css" rel="stylesheet" href="{{asset("packages/zofe/rapyd/assets/redactor/css/redactor.css")}}">
+    <!--link media="all" type="text/css" rel="stylesheet" href="{{asset("packages/zofe/rapyd/assets/redactor/css/redactor.css")}}"-->
     <link media="all" type="text/css" rel="stylesheet" href="{{asset("packages/zofe/rapyd/assets/datepicker/datepicker3.css")}}">
     <link media="all" type="text/css" rel="stylesheet" href="{{asset("packages/zofe/rapyd/assets/autocomplete/autocomplete.css")}}">
     <link media="all" type="text/css" rel="stylesheet" href="{{asset("packages/zofe/rapyd/assets/autocomplete/bootstrap-tagsinput.css")}}">
@@ -74,8 +74,8 @@
     <!-- Custom Theme JavaScript -->
     <script src="{{asset("packages/serverfireteam/panel/js/sb-admin-2.js")}}"></script>
 
-    <script src="{{asset("packages/zofe/rapyd/assets/redactor/jquery.browser.min.js")}}"></script>
-    <script src="{{asset("packages/zofe/rapyd/assets/redactor/redactor.min.js")}}"></script>
+    <!--script src="{{asset("packages/zofe/rapyd/assets/redactor/jquery.browser.min.js")}}"></script-->
+    <!--script src="{{asset("packages/zofe/rapyd/assets/redactor/redactor.min.js")}}"></script-->
     <script src="{{asset("packages/zofe/rapyd/assets/datepicker/bootstrap-datepicker.js")}}"></script>
     <script src="{{asset("packages/zofe/rapyd/assets/datepicker/locales/bootstrap-datepicker.it.js")}}"></script>
     <script src="{{asset("packages/zofe/rapyd/assets/autocomplete/typeahead.bundle.min.js")}}"></script>
