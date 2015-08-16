a7f3051e488ba15c37807dc6ac9d008f808fb9db
alenoosh <hope2end@gmail.com>
2015-08-16 15:43:34 +0430

    Elfinder file manager is added to panel but should be tested.

--- src/Serverfireteam/Panel/PanelServiceProvider.php
+++ src/Serverfireteam/Panel/PanelServiceProvider.php
@@ -12,18 +12,21 @@ use Serverfireteam\Panel\Commands;
 class PanelServiceProvider extends ServiceProvider
 {
     protected $defer = false;
-        
+
     public function register()
     {
-        // register  zofe\rapyd
+        // register zofe\rapyd
         $this->app->register('Zofe\Rapyd\RapydServiceProvider');
-        // register html service provider 
+
+        // register html service provider
         $this->app->register('Illuminate\Html\HtmlServiceProvider');
 
-       // 'Maatwebsite\Excel\ExcelServiceProvider'
-        $this->app->register('Maatwebsite\Excel\ExcelServiceProvider');        
-        
-        
+        // 'Maatwebsite\Excel\ExcelServiceProvider'
+        $this->app->register('Maatwebsite\Excel\ExcelServiceProvider');
+
+	// Barryvdh\Elfinder\ElfinderServiceProvider
+	$this->app->register('Barryvdh\Elfinder\ElfinderServiceProvider');
+
         /*
          * Create aliases for the dependency.
          */
@@ -36,51 +39,58 @@ class PanelServiceProvider extends ServiceProvider
         {
             return new \Serverfireteam\Panel\Commands\PanelCommand();
         });
-        
+
         $this->app['panel::crud'] = $this->app->share(function()
         {
             return new \Serverfireteam\Panel\Commands\CrudCommand();
         });
-        
-        
+
         $this->app['panel::createmodel'] = $this->app->share(function()
         {
            $fileSystem = new Filesystem(); 
-           
+
            return new \Serverfireteam\Panel\Commands\CreateModelCommand($fileSystem);
         });
-        
+
         $this->app['panel::createcontroller'] = $this->app->share(function()
         {
-           $fileSystem = new Filesystem(); 
-           
+           $fileSystem = new Filesystem();
+
            return new \Serverfireteam\Panel\Commands\CreateControllerPanelCommand($fileSystem);
         });
 
         $this->commands('panel::createmodel');
-        
+
         $this->commands('panel::createcontroller');
-         
+
         $this->commands('panel::install');
 
         $this->commands('panel::crud');
+
         $this->publishes([
             __DIR__ . '/../../../public' => public_path('packages/serverfireteam/panel')
         ]);
+
         $this->publishes([
             __DIR__.'/config/panel.php' => config_path('panel.php'),
         ]);
-    }
-        
-    public function boot()
-    {        
 
-     
+	// Publish assets of elfinder
+	$this->commands("elfinder:publish");
+
+	\Barryvdh\Elfinder\Console\PublishCommand(base_path() . '/vendor/barryvdh/laravel-elfinder/config/elfinder.php', config_path('elfinder.php'));
+
+	\Barryvdh\Elfinder\Console\PublishCommand(base_path() . '/vendor/barryvdh/laravel-elfinder/resources/views',
+						  base_path('resources/views/vendor/elfinder'));
+    }
+
+    public function boot()
+    {
         $this->loadViewsFrom(__DIR__.'/../../views', 'panelViews');
         $this->publishes([
             __DIR__.'/../../views' => base_path('resources/views/vendor/panelViews'),
         ]);
-        
+
         include __DIR__."/../../routes.php";
 
 	$this->loadTranslationsFrom(base_path() . '/vendor/serverfireteam/panel/src/lang', 'panel');
@@ -98,6 +108,4 @@ class PanelServiceProvider extends ServiceProvider
     {
         return array();
     }
-    
-    
 }
