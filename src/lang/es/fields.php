ebacf69faaeb66c78fb610ddc1d5f34acdab6044
Maru Amallo-amamarul <ama_marul@hotmail.com>
2016-09-15 03:30:31 -0300

    lang/es fields

--- src/lang/es/fields.php
+++ src/lang/es/fields.php
@@ -37,5 +37,9 @@ return array(
     'importDataSuccess' => 'Los datos fueron importados exitosamente',
     'importDataFailure' => 'La importación de datos falló, por favor revisa la validez de los datos',
     'successfullReset' => '¡Tu contraseña se actualizó exitosamente!',
-    'successfullEditProfile' => 'Tu perfil se actualizó exitosamente'
+    'successfullEditProfile' => 'Tu perfil se actualizó exitosamente',
+    'permissionHelp' => 'Permissions funciona con urls, con el fin de usar con ** http://Serverfire.net/panel/url/access ** , el campo Url debe ser : urlaccess  .',
+    'roleHelp' => 'Role puede tener muchos permisos.',
+    'links_help' => "El formulario 'Editar Links' es usado para agregar un link en el sidebar menu del panel. El valor del campo 'Display' especifica la etiqueta del link para ser mostrado en el menu y el valor del campo 'link' especifica el nombre del modelo.",
+    'dataSavedSuccessfull' => 'Grandioso!, información guardada, para guardar nuevamente con nueva información click en Guardar.',
 );
