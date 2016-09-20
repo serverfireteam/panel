b7eb46fece0c3150239002c6faf179f322b70ec0
Abati Adewale <acekyd01@gmail.com>
2016-09-20 23:20:34 +0100

    link_show in menu migration

--- src/database/migrations/2016_09_20_123956_edit_url_menu_table.php
+++ src/database/migrations/2016_09_20_123956_edit_url_menu_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EditUrlTable extends Migration {
+class EditUrlMenuTable extends Migration {
 
 	/**
 	 * Run the migrations.
