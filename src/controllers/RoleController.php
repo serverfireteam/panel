e28ec4e7eef501930fc29671ca652133513be93d
Morris Mukiri <morrismukiri@users.noreply.github.com>
2016-10-06 22:06:10 +0300

    change lists to pluck for 5.3 support

--- src/controllers/RoleController.php
+++ src/controllers/RoleController.php
@@ -21,7 +21,7 @@ class RoleController extends CrudController {
 		$this->grid->add('id', 'ID', true)->style("width:100px");
 		$this->grid->add('name', 'Name')->style('width:100px');
 		$this->grid->add('label', 'Description');
-		$this->grid->add('{{ implode(", ", $permissions->lists("name")->all()) }}', 'name' );
+		$this->grid->add('{{ implode(", ", $permissions->pluck("name")->all()) }}', 'name' );
 
 
 		$this->addStylesToGrid();
@@ -41,7 +41,7 @@ class RoleController extends CrudController {
 		$this->edit->link("rapyd-demo/filter", "Role", "TR")->back();
 		$this->edit->add('name', 'Name', 'text')->rule('required');
 		$this->edit->add('label', 'Description', 'text')->rule('required');
-		$this->edit->add('permissions','Permissions','checkboxgroup')->options(Permission::lists('name', 'id')->all());
+		$this->edit->add('permissions','Permissions','checkboxgroup')->options(Permission::pluck('name', 'id')->all());
 		$this->edit->saved(function () use ($entity) {
 			$this->edit->message(\Lang::get('panel::fields.dataSavedSuccessfull'));
 			$this->edit->link('panel/Permission/all', \Lang::get('panel::fields.back'));
