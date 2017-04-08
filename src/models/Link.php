cd71a0300d180344c847c3db0bda1ed10043fc8e
JM Versteeg <janmerijnversteeg@gmail.com>
2017-04-08 15:46:32 +0200

    remove unused methods from the Link model

--- src/models/Link.php
+++ src/models/Link.php
@@ -5,55 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model {
 
+    protected $fillable = ['url', 'display', 'show_menu'];
+
     protected $table = 'links';
-
-    static $cache = [];
-
-    public static function allCached($forceRefresh = false)
-    {
-        if (!isset(self::$cache['all']) || $forceRefresh) {
-            self::$cache['all'] = Link::all();
-        }
-
-        return self::$cache['all'];
-    }
-
-    public static function returnUrls($forceRefresh = false) {
-
-        if (!isset(self::$cache['all_urls']) || $forceRefresh) {
-            $configs = Link::allCached($forceRefresh);
-            self::$cache['all_urls'] =  $configs->pluck('url')->toArray();
-        }
-
-        return self::$cache['all_urls'];
-    }
-
-    public static function getMainUrls($forceRefresh = false){
-
-        if (!isset(self::$cache['main_urls']) || $forceRefresh) {
-            $configs = Link::where('main', '=', true)->get(['url']);
-            self::$cache['main_urls'] = $configs->pluck('url')->toArray();
-        }
-
-        return self::$cache['main_urls'];
-    }
-
-
-    public function getAndSave($url, $display, $show_menu = true){
-        $this->url = $url;
-        $this->display = $display;
-        $this->show_menu = $show_menu;
-        $this->save();
-    }
-
-
-    protected $fillable = array('url', 'display', 'show_menu');
-
-
-// //get roles user
-//     public function role()
-//     {
-//         return $this->hasOne('Models\Role');
-//     }
-    
 }
