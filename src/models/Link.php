0e094894bdeb89021dd6f779252c47fb8a69ef14
Reza <reza.cyber@gmail.com>
2019-06-02 15:24:41 +0430

    change function name in Link class

--- src/models/Link.php
+++ src/models/Link.php
@@ -10,28 +10,31 @@ class Link extends Model {
     protected $table = 'links';
     
     static $cache = [];
-    public static function allCached($forceRefresh = false)
+    public static function getAllLinks($forceRefresh = false) // allCached(
     {
         if (!isset(self::$cache['all']) || $forceRefresh) {
             self::$cache['all'] = Link::all();
         }
         return self::$cache['all'];
     }
-    public static function returnUrls($forceRefresh = false) {
+    public static function getUrls($forceRefresh = false) // returnUrls(
+    {
         if (!isset(self::$cache['all_urls']) || $forceRefresh) {
             $configs = Link::allCached($forceRefresh);
             self::$cache['all_urls'] =  $configs->pluck('url')->toArray();
         }
         return self::$cache['all_urls'];
     }
-    public static function getMainUrls($forceRefresh = false){
+    public static function getMainUrls($forceRefresh = false)
+    {
         if (!isset(self::$cache['main_urls']) || $forceRefresh) {
             $configs = Link::where('main', '=', true)->get(['url']);
             self::$cache['main_urls'] = $configs->pluck('url')->toArray();
         }
         return self::$cache['main_urls'];
     }
-    public function getAndSave($url, $label, $visibility){
+    public function addNewLink($url, $label, $visibility) // getAndSave(
+    {
         $this->url = $url;
         $this->display = $label;
         $this->show_menu = $visibility;
